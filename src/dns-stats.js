68a191fa74a1f9e6b1c2231b7770640ed70ccab1
Alex Sereda <sereda.san@gmail.com>
2022-10-15 14:43:28 +0300

    feat: make dns-stats task

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given an array of domains, return the object with the appearances of the DNS.
@@ -22,11 +22,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  const getDomains = (url) => {
+    return url.split(".").reduceRight((res, val) => {
+      res.push(`${res.at(-1) ?? ""}.${val}`);
+      return res;
+    }, []);
+  };
+
+  const getCounts = (arr) => {
+    return arr.reduce((res, val) => {
+      res[val] = (res[val] ?? 0) + 1;
+      return res;
+    }, {});
+  };
+
+  return getCounts(domains.flatMap(getDomains));
 }
 
 module.exports = {
-  getDNSStats
+  getDNSStats,
 };
