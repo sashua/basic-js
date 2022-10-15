bcbe3f32d37f7606bf12568bb6a9c4495ae31004
Alex Sereda <sereda.san@gmail.com>
2022-10-15 17:12:52 +0300

    feat: make mine-sweeper task

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -1,4 +1,5 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { config } = require("chai");
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
@@ -23,11 +24,44 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  const getNeighboursXY = ({ x, y, maxX, maxY }) => {
+    const neighbours = [];
+    for (const dY of [-1, 0, 1]) {
+      for (const dX of [-1, 0, 1]) {
+        const newX = x + dX;
+        const newY = y + dY;
+        if (
+          newX < 0 ||
+          newY < 0 ||
+          newX > maxX ||
+          newY > maxY ||
+          (dX === 0 && dY === 0)
+        ) {
+          continue;
+        }
+        neighbours.push([newX, newY]);
+      }
+    }
+    return neighbours;
+  };
+
+  const config = [];
+  const maxY = matrix.length - 1;
+  for (let y = 0; y <= maxY; y += 1) {
+    config.push([]);
+    const maxX = matrix[y].length - 1;
+    for (let x = 0; x <= maxX; x++) {
+      const sum = getNeighboursXY({ x, y, maxX, maxY })
+        .map(([x, y]) => matrix[y][x])
+        .reduce((sum, val) => sum + val, 0);
+
+      config[y].push(sum);
+    }
+  }
+  return config;
 }
 
 module.exports = {
-  minesweeper
+  minesweeper,
 };
